d582957badc4f572a7005d4dc5d5230d753f1ef7
Vasiliy Vanchuk <v.vanchuk@tut.by>
2018-01-18 20:16:37 +0300

    Change data structure

--- src/utils/apiWrapper.js
+++ src/utils/apiWrapper.js
@@ -1,11 +1,20 @@
+const LS_KEY = 'LS_KEY';
 let id = Date.now();
+
+let tasks;
+try {
+  tasks = JSON.parse(localStorage.getItem(LS_KEY));
+} catch (e) {
+  console.error('Error on parsing tasks', e);
+}
 
 export const getId = _ => id++;
 
-
-export const getTasks = () => new Promise(resolve => setTimeout(resolve, 0, ([
+tasks = Array.isArray(tasks) ? tasks : [
   { complited: true, title: 'Create To do app', priority: 'Normal', date: '2018-01-01' },
   { complited: false, title: 'Visit lesson', priority: 'Low', date: '2018-05-01' },
   { complited: false, title: 'Have a rest', priority: 'Normal', date: '2018-01-01' },
   { complited: true, title: 'Play with kids', priority: 'Normal', date: '2018-06-01' },
-].map(item => ({ ...item, id: getId() })))));
+].map(item => ({ ...item, id: getId() }));
+
+export const getTasks = () => new Promise(resolve => setTimeout(resolve, 0, tasks));
